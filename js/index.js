5a198a31d7c5747c226b12a925f53d6fe7d3af95
xinliuruoxi <xinliuruoxi@fox.com>
2015-05-05 22:01:04 +0800

    update.

--- js/index.js
+++ js/index.js
@@ -8,41 +8,50 @@ $(document).ready(function(){
 	$.fn.fullpage({
 		scrollingSpeed: 500,
 		css3: true,
-		scrollOverflow: true,
+		scrollOverflow: false,
 		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
 		menu: 'header,aside',
+		verticalCentered: false,
 		afterLoad: function(anchorLink, index){
 			if(index == 1){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-home').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 2){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-info').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 3){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-skill').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 4){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-mood').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 5){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-article').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 6){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-photo').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 7){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-project').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeIn();
 			}
 			if(index == 8){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-message').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
+				//$('header').fadeOut();
 			}
 		}
 	});
