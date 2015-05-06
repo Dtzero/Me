7bde1fce695e4c7771425fb174ba87ab615c065d
xinliuruoxi <xinliuruoxi@fox.com>
2015-05-06 21:49:52 +0800

    update.

--- js/index.js
+++ js/index.js
@@ -12,6 +12,17 @@ $(document).ready(function(){
 		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
 		menu: 'header,aside',
 		verticalCentered: false,
+		onLeave: function(index , nextIndex, direction){
+			if(index == 2){
+				$('#war-2-sun').fadeOut(500);
+			}
+			if(index == 3){
+				
+			}
+			if(index == 4){
+				
+			}
+		},
 		afterLoad: function(anchorLink, index){
 			if(index == 1){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
@@ -21,17 +32,25 @@ $(document).ready(function(){
 			if(index == 2){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-info').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
-				//$('header').fadeIn();
+				$('#war-2-sun').stop();
+				$('#war-2-sun').fadeIn(2000);
 			}
 			if(index == 3){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-skill').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
-				//$('header').fadeIn();
+				
+				$('#war-1-moon').animate({
+					left: '200px'
+				},800);
+				
 			}
 			if(index == 4){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
 				$('#page-mood').css({"background": "url('./images/ico_bg.png') -14px -50px no-repeat"});
-				//$('header').fadeIn();
+				
+				$('#mood-cloud').animate({
+					left: 0
+				},1000);
 			}
 			if(index == 5){
 				$('.page').css({"background": "url('./images/ico_bg.png') 0 -50px no-repeat"});
