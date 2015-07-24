ae55d0cd52d9b3ca7800754cd1d59df6e0e5647a
Tengfei Zhang <ztf@ztf.local>
2015-07-25 00:29:38 +0800

    Add Projects

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,27 @@
+(function(){
+
+})();
+
 $(document).ready(function(){
+
+	$.ajax({
+		url: './port/message.json',
+		type: 'get',
+		async: false,
+		dataType: 'json',
+		success: function(data){
+			for(var i in data.project){
+				$('body').append('<div id="war-'+ i +'" class="war war-'+ i +' section"></div>');
+			}
+		}
+	});
+	
 	//fullpage插件设置页面切换
 	$.fn.fullpage({
-		
+		scrollingSpeed: 500,
+		css3: true,
+		scrollOverflow: false,
+		//anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
+		verticalCentered: false
 	});
 });
